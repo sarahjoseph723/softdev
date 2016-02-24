18098db705e8cdbb5f1bbf1c6366f6f7847ea79b
sarahjoseph723 <sjoseph@stuy.edu>
2016-02-24 13:02:29 -0500

    fully works

--- hw02/animate.js
+++ hw02/animate.js
@@ -2,34 +2,44 @@ var c = document.getElementById("slate");
 var ctx = c.getContext("2d");
 
 ctx.fillStyle= "#FF0000";
+var frameid;
 
 var drawC = function() {
 
     var radius = 0;
     var growing = true;
     
-    if (growing) {
-	radius +=1;
-    }
-    else {
-	radius -=1;
-    }
-
-    if (radius == c.width/2) {
-	growing = false;
-    }
-
     var animCode = function() {
-	ctx.clearRect(0,0,c.width,c.height);
+
+        ctx.clearRect(0,0,c.width,c.height);
+	
+	if (growing) {
+	    radius +=1;
+	}
+	else {
+	    radius -=1;
+	}
+	if (radius == 250) {
+	    growing = false;
+	}
+	if (radius == 0) {
+	    growing = true;
+	}
 	ctx.beginPath();
 	ctx.arc(250,250,radius, 0, 2*Math.PI);
 	ctx.fill();
-	radius += 1;
-	window.requestAnimationFrame(animCode);
+	frameid = window.requestAnimationFrame(animCode);
     };
 
     animCode();
 };
+
+var stopIt = function() {
+    window.cancelAnimationFrame(frameid)
+};
+
+var s = document.getElementById("s");
+s.addEventListener("click", stopIt);
 
 var b = document.getElementById("b");
 b.addEventListener("click",drawC);
